9dec77cc6e68238ddd64d82e927837be26fb87d7
24124s1 <rpica640@gmail.com>
2025-07-12 03:32:58 -0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -74,46 +74,6 @@ getgenv().hitboxChances = getgenv().hitboxChances or {
     ["Right Arm"] = 100
 }
 
-Saim:AddSeparator({
-    enabled = true,
-    text = "Must Add Up to 100 or Breaks"
-})
-Saim:AddSeparator({
-    enabled = true,
-    text = "|No Over|"
-})
-
-
-local function getSumExcept(exceptHitbox)
-    local sum = 0
-    for _, hitbox in ipairs(realHitboxes) do
-        if hitbox ~= exceptHitbox then
-            sum = sum + (getgenv().hitboxChances[hitbox] or 0)
-        end
-    end
-    return sum
-end
-
-for _, hitboxName in ipairs(realHitboxes) do
-    Saim:AddSlider({
-        text = hitboxName .. " Chance",
-        flag = "Chance_" .. hitboxName:gsub(" ", ""),
-        min = 0,
-        max = 100,
-        increment = 1,
-        value = getgenv().hitboxChances[hitboxName],
-        callback = function(v)
-            getgenv().hitboxChances[hitboxName] = v
-        end,
-        get = function()
-            local currentVal = getgenv().hitboxChances[hitboxName]
-            local maxVal = 100 - getSumExcept(hitboxName)
-            if maxVal < 0 then maxVal = 0 end
-            return currentVal, min(100, maxVal)
-        end
-    })
-end
-
 local realHitboxes = {
     "Head",
     "Torso",
